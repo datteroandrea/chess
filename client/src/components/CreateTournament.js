b3fdbaef60aa5b98b914e9c2e2d196fb0a4e57b5
datteroandrea <dattero.andrea@gmail.com>
2022-02-10 15:02:21 +0100

    added CreateTournament page

---
+++ client/src/components/CreateTournament.js
@@ -0,0 +1,24 @@
+import "./styles/Home.css";
+import { Component } from "react";
+import { Link } from "react-router-dom";
+//import axios from 'axios';
+
+export default class CreateTournament extends Component {
+
+    componentDidMount() {
+        
+    }
+
+    render() {
+        return <div>
+            <div>
+                { /** qui mettiamo la scacchiera di gioco */}
+            </div>
+            <div>
+                { /** qui mettiamo il pannello con tutte le impostazioni del torneo ad esempio:
+                 * la durata, il tipo di torneo */ }
+            </div>
+        </div>;
+    }
+
+}
