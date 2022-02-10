527063a70b6523265d19c22cdf9f3dfc97923b06
datteroandrea <dattero.andrea@gmail.com>
2022-02-10 15:02:10 +0100

    added CreateRoom page

---
+++ client/src/components/CreateRoom.js
@@ -0,0 +1,24 @@
+import "./styles/Home.css";
+import { Component } from "react";
+import { Link } from "react-router-dom";
+//import axios from 'axios';
+
+export default class CreateRoom extends Component {
+
+    componentDidMount() {
+        
+    }
+
+    render() {
+        return <div>
+            <div>
+                { /** qui mettiamo la scacchiera di gioco */}
+            </div>
+            <div>
+                { /** qui mettiamo il pannello con tutte le impostazioni della stanza ad esempio:
+                 * pubblica/privata,  */ }
+            </div>
+        </div>;
+    }
+
+}
