b350ab5adec73678c40b2c41dacf3cfb02bb1049
Giovanni Stevanato <stevanay@gmail.com>
2022-02-11 18:16:58 +0100

    fen load input added (to fix)

--- client/src/components/FreeBoard.js
+++ client/src/components/FreeBoard.js
@@ -22,11 +22,18 @@ export default class FreeBoard extends Component {
             </div>
 
             <div className="StockfishContainer">
-                STOCKFISH
+                <h3>STOCKFISH</h3>
             </div>
 
             <div className="NavigatePositionContainer">
-                NAVIGATE POSITION
+                <h3>NAVIGATE POSITION</h3>
+                <div className="input-group">
+                    <span className="input-group-text"><i className="fa fa-lock fa-fw"></i></span>
+                    <input type="text" className="form-control" placeholder="FEN string..." aria-label="FEN string..." aria-describedby="basic-addon2"></input>
+                    <div className="input-group-append">
+                        <button className="btn btn-outline-secondary btn-small" type="button">Load FEN</button>
+                    </div>
+                </div>
             </div>
 
         </div>;
