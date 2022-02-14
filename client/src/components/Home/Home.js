77c90134536d0024c32eaca78450117319c49bad
datteroandrea <dattero.andrea@gmail.com>
2022-02-14 19:45:54 +0100

    added request to games route and improved play button

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,23 +1,26 @@
 import "./Home.css";
 import { Component } from "react";
 import { Link } from "react-router-dom";
-//import axios from 'axios';
+import axios from 'axios';
 
 export default class Home extends Component {
 
     constructor(props) {
         super(props);
+        this.state = {};
     }
 
     componentDidMount() {
-        
+        this.games();
     }
 
-
+    async games() {
+        this.setState({ game: await axios.get("http://localhost:4000/games") });
+    }
 
     render() {
         return <div className="center">
-            <button className="btn btn-primary btn-lg btn-block" onClick={() => { window.location.replace("/game/create") }}>Play a game!</button>
+            <button className="btn btn-lg game-btn" onClick={() => { window.location.replace("/games/create") }}><i className="fa fa-play fa-fw"></i> Play a game!</button>
         </div>;
     }
 
