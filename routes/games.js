8e888c99f926f85225677f5106dde2fe88bf9d0d
datteroandrea <dattero.andrea@gmail.com>
2022-02-14 19:46:29 +0100

    added games routes

---
+++ routes/games.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+const Game = require('../models/game');
+const crypto = require('crypto');
+const router = new express.Router();
+
+router.get('/', async (req, res) => {
+    let games = await Game.find({ isStarted: false });
+    console.log(games);
+    res.send(games);
+});
+
+router.get('/create', async (req, res) => {
+    let token = jwt.decode(req.token);
+    let game = req.body;
+    let isWhite = Math.floor(Math.random());
+
+    game.gameId = crypto.randomUUID();
+    if (isWhite >= 0.5) {
+        game.whitePlayerId = token.user_id;
+        game.blackPlayerId = "";
+    } else {
+        game.whitePlayerId = "";
+        game.blackPlayerId = token.user_id;
+    }
+
+    game.pgn = [];
+    game.winnerId = "";
+
+    console.log(game);
+
+    await Game.create(game);
+    res.send({ game });
+});
+
+module.exports = router;
