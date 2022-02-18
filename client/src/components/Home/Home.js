9be3b0267e94c72c340eb9ba9be95cace316c1ad
datteroandrea <dattero.andrea@gmail.com>
2022-02-18 18:01:46 +0100

    code improvements

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -23,14 +23,14 @@ export default class Home extends Component {
 
     render() {
         return <div className="center">
-            <Link className="btn btn-lg game-btn" to="/games/create"><i className="fa fa-play fa-fw"></i> Play a game!</Link>
-            <div style={{marginTop: 20}}>
+            <Link className="btn btn-lg create-game-btn" to="/games/create"><i className="fa fa-play fa-fw"></i> Create game!</Link>
+            <div className="game-list">
                 {this.state.games.map((game) => {
                     return <div className="row">
                         <div className="col col-2">Play as { game.blackPlayerId? "white" : "black" }</div>
                         <div className="col col-2">{ game.timeLimit } minutes</div>
                         <div className="col col-2">{ game.isRated ? "Rated" : "Unrated" }</div>
-                        <div className="col col-4"><Link className="btn btn-lg" to={ "/games/create/"+game.gameId }><i className="fa fa-play fa-fw"></i> Play a game!</Link></div>
+                        <div className="col col-4"><Link className="btn btn-lg play-game-btn" to={ "/games/create/"+game.gameId }><i className="fa fa-play fa-fw"></i> Play a game!</Link></div>
                     </div>
                 })}
             </div>
