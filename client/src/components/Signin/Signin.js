1eaf69dfdbee09c6b955f0d77ca6488642ebe855
datteroandrea <dattero.andrea@gmail.com>
2022-02-18 15:40:28 +0100

    fixed bugs

--- client/src/components/Signin/Signin.js
+++ client/src/components/Signin/Signin.js
@@ -19,20 +19,21 @@ export default class Signin extends Component {
         axios.post("http://localhost:4000/auth/sign-in", account).then((token) => {
             //console.log(jwtDecode(token.data));
             localStorage.setItem("token", token.data);
-            
+
             window.location.replace("/");
         });
     }
 
-    setPasswordVisibility(element) {
-        let hideShow = element.children[2].children[0];
-        let passwordInput = element.children[1];
-
-        if (hideShow.className == "fa fa-eye fa-fw") {
-            hideShow.className = "fa fa-eye-slash fa-fw";
+    setPasswordVisibility(event) {
+        console.log(event.target);
+        let hideShow = event.target;
+        let passwordInput = event.target.parentElement.parentElement.previousSibling;
+        console.log(passwordInput);
+        if (hideShow.src.includes("eye-solid.svg")) {
+            hideShow.src = "./Assets/icons/eye-slash-solid.svg";
             passwordInput.type = "password";
         } else {
-            hideShow.className = "fa fa-eye fa-fw";
+            hideShow.src = "./Assets/icons/eye-solid.svg";
             passwordInput.type = "text";
         }
     }
@@ -44,7 +45,7 @@ export default class Signin extends Component {
     render() {
         return <div className="signin center">
             <a className="btn btn-lg btn-block btn-outline googlebutton">
-                <img src="https://img.icons8.com/color/16/000000/google-logo.png" /> Signin with Google
+                <img src="./Assets/icons/google-logo.png" /> Signin with Google
             </a>
             <div className="or-container">
                 <div className="line-separator"></div>
@@ -54,20 +55,29 @@ export default class Signin extends Component {
             <div className="form-group">
                 <p className="label">Email Address</p>
                 <div className="input-group">
-                    <span className="input-group-text"><i className="fa fa-envelope fa-fw"></i></span>
+                    <span className="input-group-text bg-transparent  border-0" id="basic-addon1">
+                        <img src="./Assets/icons/envelope-solid.svg" style={{ width: 16, height: 16 }}></img>
+                    </span>
                     <input id="email" name="email" className="form-control" type="text" placeholder="Email Address" />
                 </div>
             </div>
             <div className="form-group">
                 <p className="label">Password</p>
                 <div className="input-group">
-                    <span className="input-group-text"><i className="fa fa-lock fa-fw"></i></span>
+                    <div className="input-group-prepend bg-transparent">
+                        <span className="input-group-text bg-transparent  border-0" id="basic-addon1">
+                            <img src="./Assets/icons/lock-solid.svg" style={{ width: 16, height: 16 }}></img>
+                        </span>
+                    </div>
                     <input id="password" className="form-control" type="password" placeholder="Password" />
-                    <span className="input-group-text"><i id="hideShow" onClick={(e) => { this.setPasswordVisibility(e.target.parentElement.parentElement) }}
-                        className="fa fa-eye-slash fa-fw"></i></span>
+                    <div className="input-group-append bg-transparent" onClick={this.setPasswordVisibility}>
+                        <span className="input-group-text bg-transparent  border-0" id="basic-addon1">
+                            <img src="./Assets/icons/eye-slash-solid.svg" style={{ width: 16, height: 16 }}></img>
+                        </span>
+                    </div>
                 </div>
             </div>
-            <button className="btn btn-outline-success signinbutton" onClick={this.signin}>Signin</button>
+            <button style={{marginTop: 20}} className="btn btn-outline-success signinbutton" onClick={this.signin}>Signin</button>
             <div>
                 <Link className="signup" to="/sign-up">
                     Signup
