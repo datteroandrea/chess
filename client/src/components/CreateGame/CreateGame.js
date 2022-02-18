676ae57184da1739570922b58ea5c52eec7449a2
datteroandrea <dattero.andrea@gmail.com>
2022-02-18 18:01:08 +0100

    added sidebar logic

--- client/src/components/CreateGame/CreateGame.js
+++ client/src/components/CreateGame/CreateGame.js
@@ -9,19 +9,56 @@ export default class CreateGame extends Component {
     constructor(props) {
         super(props);
         this.board = React.createRef();
+        this.state = {
+            game: {
+                time: 30,
+                vs: "Player",
+                isRated: true,
+                color: "white"
+            }
+        }
     }
 
     componentDidMount() {
-        console.log(localStorage.getItem("token"));
+
     }
 
     createGame() {
-        axios.post("http://localhost:4000/games/create", {
-            isRated: true,
-            timeLimit: 20
-        }).then((game) => {
-            window.location.replace("/games/" + game.game_id);
-        });
+        if (this.state.game.vs === "Player") {
+            axios.post("http://localhost:4000/games/create", {
+                isRated: true,
+                timeLimit: 20
+            }).then((game) => {
+                window.location.replace("/games/" + game.game_id);
+            });
+        }
+    }
+
+    handleAccordion(e) {
+        if (e.target.nextElementSibling.getAttribute("class").includes("show"))
+            e.target.nextElementSibling.setAttribute("class", "accordion-collapse collapse hide")
+        else
+            e.target.nextElementSibling.setAttribute("class", "accordion-collapse collapse show")
+    }
+
+    handlePvP() {
+        this.state.game.vs = (this.state.game.vs == "Player") ? "Computer" : "Player";
+        this.setState({});
+    }
+
+    handleRated() {
+        this.state.game.isRated = (this.state.game.isRated) ? false : true;
+        this.setState({});
+    }
+
+    handleColor() {
+        this.state.game.color = (this.state.game.color == "white") ? "black" : "white";
+        this.setState({});
+    }
+
+    handleTime(e) {
+        this.state.game.time = e.target.value;
+        this.setState({});
     }
 
     render() {
@@ -31,15 +68,28 @@ export default class CreateGame extends Component {
                     <Chessboard ref={this.board} />
                 </div>
                 <div className="col col-2">
-                    { /** qui mettiamo il pannello con tutte le impostazioni della partità ad esempio:
-                 * la durata, partità PvP o PvE ecc. */ }
-                    <button className="btn btn-lg time-btn"><span><i className="fa fas fa-hourglass fa-fw"></i></span> 30 min</button>
-                    <button className="btn btn-lg opponent-btn"><span><i className="fa fa-user fa-fw"></i></span> PvP</button>
-                    { /** Il bottone Rated va mostrato soltanto su partite PvP  */}
-                    <button className="btn btn-lg opponent-btn"><span><i className="fa fa-trophy fa-fw"></i></span> Rated</button>
-                    { /** Il bottone Color va mostrato soltanto su partite PvE fai che il colore del bottone diventa bianco o nero  */}
-                    <button className="btn btn-lg opponent-btn"><span><i className="fa fa-flag fa-fw"></i></span> Color</button>
-                    <button className="btn btn-lg play-btn" onClick={this.createGame}>Create Game</button>
+                    <div class="accordion accordion-flush">
+                        <div class="accordion-item">
+                            <button className="btn btn-lg time-btn" onClick={this.handleAccordion}><span><i className="fa fas fa-hourglass fa-fw"></i></span> {this.state.game.time} min</button>
+                            <div className="accordion-collapse collapse hide">
+                                <div class="accordion-body" style={{ width: 300 }}>
+                                    <div className="input-group">
+                                        <span className="input-group-text bg-transparent border-0" id="basic-addon1">
+                                            <img src="../Assets/icons/time.svg" style={{ width: 16, height: 16 }}></img>
+                                        </span>
+                                        <input id="time" name="time" className="form-control" type="number" placeholder="time" min="1" defaultValue={30} onChange={(e) => { this.handleTime(e) }} />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <button className="btn btn-lg opponent-btn" onClick={() => { this.handlePvP() }}><span><i className="fa fa-user fa-fw"></i></span> { (this.state.game.vs == "Player") ? "Player" : "Computer" }</button>
+                    { this.state.game.vs == "Player" ? <button className="btn btn-lg opponent-btn" onClick={() => { this.handleRated() }}><span><i className="fa fa-trophy fa-fw"></i></span> {(this.state.game.isRated) ? "Rated" : "Unrated"}</button> : null }
+                    { this.state.game.vs == "Computer" ? <button className="btn btn-lg opponent-btn" onClick={() => { this.handleColor() }}><span><i className="fa fa-flag fa-fw"></i></span> {(this.state.game.color == "white") ? "White" : "Black"}</button> : null }
+
+                    <div class="accordion-item">
+                        <button className="btn btn-lg play-btn" onClick={() => { this.createGame() }}>Create Game</button>
+                    </div>
                 </div>
             </div>
         </div>;
