db93516f058c7401fe7f68f3564242a4533161ec
datteroandrea <dattero.andrea@gmail.com>
2022-02-19 15:44:51 +0100

    trying websocket connection

--- client/src/components/MultiplayerGame/MultiplayerGame.js
+++ client/src/components/MultiplayerGame/MultiplayerGame.js
@@ -3,7 +3,12 @@ import { Component } from "react";
 export default class MultiplayerGame extends Component {
 
     componentDidMount() {
+        const ws = new WebSocket("ws://locahost:4001");
+        ws.onopen = (event) => {
+            ws.send("message\n");
+        };
 
+        console.log(ws);
     }
 
     render() {
