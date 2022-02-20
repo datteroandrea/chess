0328345ff913e7803ae1f9a212f32f1b846c2dfa
datteroandrea <dattero.andrea@gmail.com>
2022-02-20 14:10:36 +0100

    added Chessboard

--- client/src/components/MultiplayerGame/MultiplayerGame.js
+++ client/src/components/MultiplayerGame/MultiplayerGame.js
@@ -1,18 +1,39 @@
 import { Component } from "react";
+import React from "react";
+import Chessboard from "../Chessboard/Chessboard";
+import "./MultiplayerGame.css";
 
 export default class MultiplayerGame extends Component {
 
+    constructor(props) {
+        super(props);
+        this.board = React.createRef();
+        this.socket = new WebSocket("ws://127.0.0.1:4001");
+    }
+
     componentDidMount() {
-        const ws = new WebSocket("ws://127.0.0.1:4001");
-        ws.onopen = (event) => {
-            ws.send("message");
+        this.socket.onopen = (event) => {
+            this.socket.send(JSON.stringify({
+                token: localStorage.getItem("token")
+            }));
         };
 
-        console.log(ws);
+        this.socket.onmessage = (event) => {
+            console.log(event.data);
+        }
     }
 
     render() {
-        return <div></div>;
+        return <div className="row">
+            <div className="col col-8">
+                <Chessboard ref={this.board} />
+            </div>
+            <div className="col col-2">
+                <div>
+                    
+                </div>
+            </div>
+        </div>;
     }
 
 }
