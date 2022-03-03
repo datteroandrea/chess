b6abc666cc9a42eaa47a5c58dd36dfa1b9833a26
Giovanni Stevanato <stevanay@gmail.com>
2022-03-03 15:23:13 +0100

    started implementation of stockfish settings pop up modal

---
+++ client/src/components/FreeBoard/SettingsGear/SettingsGear.js
@@ -0,0 +1,16 @@
+import "./SettingsGearStyle.css";
+import React from "react";
+
+const { Component } = React;
+
+export default class SettingsGear extends Component {
+
+    render() {
+
+        return  <span className="settings">
+                    <div className="settingsGear"></div>
+                </span>
+
+    }
+
+}
