c725cd73f50188af8d2e99719b483b08bf016b09
datteroandrea <dattero.andrea@gmail.com>
2022-03-03 13:12:34 +0100

    fixed button overlapping on small screens

--- client/src/components/CreateGame/CreateGame.js
+++ client/src/components/CreateGame/CreateGame.js
@@ -3,6 +3,7 @@ import React from "react";
 import { Component } from "react";
 import Chessboard from "../Chessboard/Chessboard";
 import axios from 'axios';
+import MediaQuery from 'react-responsive';
 
 export default class CreateGame extends Component {
 
@@ -34,11 +35,11 @@ export default class CreateGame extends Component {
         }
     }
 
-    handleAccordion(e) {
+    handleTimeBox(e) {
         if (e.target.nextElementSibling.getAttribute("class").includes("show"))
-            e.target.nextElementSibling.setAttribute("class", "accordion-collapse collapse hide")
+            e.target.nextElementSibling.setAttribute("class", "time-box hide")
         else
-            e.target.nextElementSibling.setAttribute("class", "accordion-collapse collapse show")
+            e.target.nextElementSibling.setAttribute("class", "time-box show")
     }
 
     handlePvP() {
@@ -63,16 +64,39 @@ export default class CreateGame extends Component {
 
     render() {
         return <div className="container">
-            <div className="row">
-                <div className="col col-8">
-                    <Chessboard ref={this.board} />
-                </div>
-                <div className="col col-2">
-                    <div className="accordion accordion-flush">
-                        <div className="accordion-item">
-                            <button className="btn btn-lg time-btn" onClick={this.handleAccordion}><span><i className="fa fas fa-hourglass fa-fw"></i></span> {this.state.game.time} min</button>
-                            <div className="accordion-collapse collapse hide">
-                                <div className="accordion-body" style={{ width: 300 }}>
+            <MediaQuery maxWidth={1200} style={{ width: 300 }}>
+                <ul class="list-group">
+                    <li class="list-group-item" style={{ width: 340 }}>
+                        <button className="btn btn-lg time-btn" onClick={this.handleTimeBox}><span><i style={{ height: 20 }} className="fa fas fa-hourglass fa-fw"></i></span> {this.state.game.time} min</button>
+                        <div className="time-box hide">
+                            <div className="input-group">
+                                <span className="input-group-text bg-transparent border-0" id="basic-addon1">
+                                    <img src="../Assets/icons/time.svg" style={{ width: 16, height: 16 }}></img>
+                                </span>
+                                <input id="time" name="time" className="form-control" type="number" placeholder="time" min="1" defaultValue={30} onChange={(e) => { this.handleTime(e) }} />
+                            </div>
+                        </div>
+                    </li>
+                    <li class="list-group-item">
+                        <button className="btn btn-lg opponent-btn" onClick={() => { this.handlePvP() }}><span><i className="fa fa-user fa-fw"></i></span> {(this.state.game.vs == "Player") ? "Player" : "Computer"}</button>
+                    </li>
+                    {this.state.game.vs == "Player" ?<li class="list-group-item"><button className="btn btn-lg opponent-btn" onClick={() => { this.handleRated() }}><span><i className="fa fa-trophy fa-fw"></i></span> {(this.state.game.isRated) ? "Rated" : "Unrated"}</button></li> : null}
+                    {this.state.game.vs == "Computer" ? <li class="list-group-item"><button className="btn btn-lg opponent-btn" onClick={() => { this.handleColor() }}><span><i className="fa fa-flag fa-fw"></i></span> {(this.state.game.color == "white") ? "White" : "Black"}</button></li> : null}
+                    <li class="list-group-item">
+                        <button className="btn btn-lg play-btn" onClick={() => { this.createGame() }}>Create Game</button>
+                    </li>
+                </ul>
+            </MediaQuery>
+            <MediaQuery minWidth={1201}>
+                <div className='row'>
+                    <div className="col col-8">
+                        <Chessboard ref={this.board} />
+                    </div>
+                    <div className="col col-4">
+                        <ul class="list-group">
+                            <li class="list-group-item">
+                                <button className="btn btn-lg time-btn" onClick={this.handleTimeBox}><span><i className="fa fas fa-hourglass fa-fw"></i></span> {this.state.game.time} min</button>
+                                <div className="time-box hide">
                                     <div className="input-group">
                                         <span className="input-group-text bg-transparent border-0" id="basic-addon1">
                                             <img src="../Assets/icons/time.svg" style={{ width: 16, height: 16 }}></img>
@@ -80,18 +104,18 @@ export default class CreateGame extends Component {
                                         <input id="time" name="time" className="form-control" type="number" placeholder="time" min="1" defaultValue={30} onChange={(e) => { this.handleTime(e) }} />
                                     </div>
                                 </div>
-                            </div>
-                        </div>
-                    </div>
-                    <button className="btn btn-lg opponent-btn" onClick={() => { this.handlePvP() }}><span><i className="fa fa-user fa-fw"></i></span> { (this.state.game.vs == "Player") ? "Player" : "Computer" }</button>
-                    { this.state.game.vs == "Player" ? <button className="btn btn-lg opponent-btn" onClick={() => { this.handleRated() }}><span><i className="fa fa-trophy fa-fw"></i></span> {(this.state.game.isRated) ? "Rated" : "Unrated"}</button> : null }
-                    { this.state.game.vs == "Computer" ? <button className="btn btn-lg opponent-btn" onClick={() => { this.handleColor() }}><span><i className="fa fa-flag fa-fw"></i></span> {(this.state.game.color == "white") ? "White" : "Black"}</button> : null }
-
-                    <div className="accordion-item">
-                        <button className="btn btn-lg play-btn" onClick={() => { this.createGame() }}>Create Game</button>
+                            </li>
+                            <li class="list-group-item">
+                                <button className="btn btn-lg opponent-btn" onClick={() => { this.handlePvP() }}><span><i className="fa fa-user fa-fw"></i></span> {(this.state.game.vs == "Player") ? "Player" : "Computer"}</button>
+                            </li>
+                            {this.state.game.vs == "Player" ? <li class="list-group-item"><button className="btn btn-lg opponent-btn" onClick={() => { this.handleRated() }}><span><i className="fa fa-trophy fa-fw"></i></span> {(this.state.game.isRated) ? "Rated" : "Unrated"}</button></li> : null}
+                            {this.state.game.vs == "Computer" ? <li class="list-group-item"><button className="btn btn-lg opponent-btn" onClick={() => { this.handleColor() }}><span><i className="fa fa-flag fa-fw"></i></span> {(this.state.game.color == "white") ? "White" : "Black"}</button></li> : null}
+                            <li class="list-group-item"><button className="btn btn-lg play-btn" onClick={() => { this.createGame() }}>Create Game</button></li>
+                        </ul>
                     </div>
                 </div>
-            </div>
+            </MediaQuery>
+
         </div>;
     }
 
