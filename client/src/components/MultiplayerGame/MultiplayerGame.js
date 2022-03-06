ec6b65e9d989661d4eee5ce95b893b0d271b2637
datteroandrea <dattero.andrea@gmail.com>
2022-03-06 19:06:53 +0100

    fixed bugs

--- client/src/components/MultiplayerGame/MultiplayerGame.js
+++ client/src/components/MultiplayerGame/MultiplayerGame.js
@@ -15,30 +15,35 @@ export default class MultiplayerGame extends Component {
         this.board = React.createRef();
         this.timer = React.createRef();
         this.state = {
-            playerColor: "white"
+            playerColor: "white",
+            hasWon: null
         };
     }
 
     async componentDidMount() {
-        this.token = localStorage.getItem("token");
-        this.gameId = window.location.pathname.split("/")[2];
-
-        let userId = jwtDecode(this.token).user_id;
-
-        let game = await axios.post("/games/" + this.gameId + "/play");
-
-        game.data.moves.forEach((move) => {
-            let promotion = move.substring(4, 5);
-            this.board.current.makeMove(move.substring(0, 2), move.substring(2, 4), promotion, false);
-        });
-
         this.socket = new WebSocket("ws://" + Config.address + ':8001');
 
-        this.socket.onopen = (event) => {
+        this.socket.onopen = async (event) => {
             this.socket.send(JSON.stringify({
                 token: this.token,
                 gameId: this.gameId
             }));
+
+            let game = (await axios.post("/games/" + this.gameId + "/play")).data;
+
+            game.moves.forEach((move) => {
+                let promotion = move.substring(4, 5);
+                this.board.current.makeMove(move.substring(0, 2), move.substring(2, 4), promotion, false);
+            });
+
+            this.setState({
+                game: game,
+                playerColor: this.userId === game.blackPlayerId ? "black" : "white"
+            });
+
+            if (this.state.playerColor === "black") {
+                this.board.current.rotateBoard();
+            }
         };
 
         this.socket.onmessage = (event) => {
@@ -46,23 +51,22 @@ export default class MultiplayerGame extends Component {
             if (message.type === "move") {
                 let promotion = message.move.substring(4, 5);
                 this.board.current.makeMove(message.move.substring(0, 2), message.move.substring(2, 4), promotion, false);
-                this.timer.current.startTimer();
+                this.timer.current.startTimer(message.time);
+            }
+
+            if (message.type === "win") {
+                console.log(message);
             }
         }
-
-        this.setState({
-            game: game.data,
-            playerColor: userId === game.data.blackPlayerId ? "black" : "white",
-            time: userId === game.data.blackPlayerId ? game.data.blackPlayerTime : game.data.whitePlayerTime
-        });
     }
 
     render() {
-        if(this.state.playerColor === "black") {
-            this.board.current.rotateBoard();
-        }
+        this.token = localStorage.getItem("token");
+        this.gameId = window.location.pathname.split("/")[2];
+        this.userId = jwtDecode(this.token).user_id;
+
         return <div style={{ marginLeft: 20 }}>
-            { this.state.time? <Timer ref={this.timer} minutes={Math.floor(this.state.time / 60)} seconds={Math.floor(this.state.time % 60)}></Timer> : null }
+            <Timer ref={this.timer} userId={this.userId} gameId={this.gameId}></Timer>
             <Chessboard ref={this.board} playerColor={this.state.playerColor} onMove={(move) => {
                 this.socket.send(JSON.stringify({
                     token: this.token,
