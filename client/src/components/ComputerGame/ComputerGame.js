892903acee6c3105d3cd7c89f65195a59f9d3986
Giovanni Stevanato <stevanay@gmail.com>
2022-03-06 18:09:01 +0100

    implemented 10 levels of stockfish settings and added set level modal

--- client/src/components/ComputerGame/ComputerGame.js
+++ client/src/components/ComputerGame/ComputerGame.js
@@ -2,12 +2,28 @@ import './ComputerGame.css';
 import React from "react";
 import { Component } from "react";
 import Chessboard from "../Chessboard/Chessboard";
+import SetLevelModal from './SetLevelModal/SetLevelModal';
+
+const levels = {
+    1: {skill: 0, depth:1},
+    2: {skill: 1, depth:2},
+    3: {skill: 2, depth:3},
+    4: {skill: 3, depth:3},
+    5: {skill: 6, depth:5},
+    6: {skill: 9, depth:7},
+    7: {skill: 10, depth:10},
+    8: {skill: 12, depth:12},
+    9: {skill: 16, depth:16},
+    10: {skill: 20, depth:20}
+}
 
 export default class ComputerGame extends Component {
 
     constructor(props) {
         super(props);
         this.board = React.createRef();
+        this.levelModal = React.createRef();
+        this.levelLabel = React.createRef();
         this.color = window.location.pathname.split("/")[3];
         this.level = window.location.pathname.split("/")[4];
     }
@@ -17,9 +33,7 @@ export default class ComputerGame extends Component {
                     <div className='boardContainer'>
                         <Chessboard ref={this.board} playerColor={this.color} 
                         onMove={(_, fen) => {
-                            this.stockfish.postMessage("stop");
-                            this.stockfish.postMessage("position fen " + fen);
-                            this.stockfish.postMessage("go depth 16");
+                            this.triggerStockfish(fen);
                         }}
                         onGameRestart={() => {
                             this.board.current.rotateBoard();
@@ -27,19 +41,19 @@ export default class ComputerGame extends Component {
                                 this.color = "white";
                             }else{
                                 this.color = "black";
-                                this.stockfish.postMessage("position startpos");
-                                this.stockfish.postMessage("go depth 16");
+                                this.triggerStockfish("startpos");
                             }
                         }}/>
                     </div>
                     <div className='computerSettingsContainer'>
-                        <div className="containerTitle">COMPUTER GAME (level: {this.level})</div>
+                        <div className="containerTitle">COMPUTER GAME (level: <label ref={this.levelLabel}>{this.level}</label> )</div>
                         <div className="multi-button2">
                             <button onClick={() => this.board.current.endGame(this.color === "white" ? "black" : "white", "surrender")} className="mbutton2"><img src="../../../Assets/icons/surrender.svg" alt="surrender" className="img_icon"></img>Surrender</button>
-                            <button onClick={() => this.setLevel()} className="mbutton2"><img src="../../../Assets/icons/sliders.svg" alt="level" className="img_icon"></img>Level</button>
+                            <button onClick={() => this.levelModal.current.enable()} className="mbutton2"><img src="../../../Assets/icons/sliders.svg" alt="level" className="img_icon"></img>Level</button>
                             <button onClick={() => this.analyze()} className="mbutton2"><img src="../../../Assets/icons/analyze.svg" alt="analyze" className="img_icon"></img>Analyze</button>
                         </div>
                     </div>
+                    <SetLevelModal ref={this.levelModal} level={this.level} onLevelChange={(level) => this.setLevel(level)}></SetLevelModal>
                 </div>;
     }
 
@@ -54,21 +68,35 @@ export default class ComputerGame extends Component {
             this.playStockfishMove(e.data);
         };
 
+        this.stockfish.postMessage('setoption name Skill Level value ' + levels[this.level].skill);
+
         if(this.color === "black"){
-            this.stockfish.postMessage("position startpos");
-            this.stockfish.postMessage("go depth 16");
+            this.triggerStockfish("startpos")
         }
     }
 
+    triggerStockfish(fen){
+        this.stockfish.postMessage("stop");
+        if(fen === "startpos"){
+            this.stockfish.postMessage("position startpos");
+        }else{
+            this.stockfish.postMessage("position fen " + fen);
+        }
+        this.stockfish.postMessage("go depth " + levels[this.level].depth);
+    }
+
     playStockfishMove(msg){
+        console.log(msg);
         if(msg.startsWith("bestmove")){
             let move = msg.split(" ")[1];
             this.board.current.makeMove(move.substring(0,2), move.substring(2,4), move[4]);
         }
     }
 
-    setLevel(){
-
+    setLevel(level){
+        this.level = level;
+        this.stockfish.postMessage('setoption name Skill Level value ' + levels[this.level].skill);
+        this.levelLabel.current.innerHTML = level;
     }
 
     analyze(){
