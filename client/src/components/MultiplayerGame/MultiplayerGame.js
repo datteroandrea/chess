360e51a029ca0a8ae213fef723beea3297aacd00
datteroandrea <dattero.andrea@gmail.com>
2022-03-13 13:22:14 +0100

    enabled https

--- client/src/components/MultiplayerGame/MultiplayerGame.js
+++ client/src/components/MultiplayerGame/MultiplayerGame.js
@@ -20,7 +20,7 @@ export default class MultiplayerGame extends Component {
     }
 
     async componentDidMount() {
-        this.socket = new WebSocket("ws://" + Config.address + ':8001');
+        this.socket = new WebSocket("wss://" + Config.address + ':8001');
 
         this.socket.onopen = async (event) => {
             let game = (await axios.post("/games/" + this.gameId + "/play")).data;
