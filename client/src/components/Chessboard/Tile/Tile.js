d3b7ad92a20e7ebcebc4ce90609641334b0c3b73
Giovanni Stevanato <stevanay@gmail.com>
2022-03-21 10:58:22 +0100

    reworked tiles square names to be on the left and bottom sides

--- client/src/components/Chessboard/Tile/Tile.js
+++ client/src/components/Chessboard/Tile/Tile.js
@@ -13,7 +13,12 @@ export default function Tile(props) {
     }
 
     return <div id ={props.squareName} className={squareColor}>
-                <p>{props.squareName}</p>
+                <p>
+                {props.squareName[0] === "a" ? <span className="topLeftsquareName">{props.squareName[1]}</span> : null}
+                {props.squareName[1] === "1" ? <span className="bottomRightsquareName">{props.squareName[0]}</span> : null}
+                {props.squareName[0] === "h" ? <span className="topLeftsquareName Rotated">{props.squareName[1]}</span> : null}
+                {props.squareName[1] === "8" ? <span className="bottomRightsquareName Rotated">{props.squareName[0]}</span> : null}
+                </p>
                 {props.pieceName ? <Piece pieceName={props.pieceName} /> : null}
             </div>;
 
