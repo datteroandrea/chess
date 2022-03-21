3cf78723b90770d10b3d39b0a0abe104027a5217
datteroandrea <dattero.andrea@gmail.com>
2022-03-21 19:31:17 +0100

    improved UI

--- client/src/components/Room/Room.js
+++ client/src/components/Room/Room.js
@@ -9,16 +9,42 @@ export default class Room extends Component {
     constructor(props) {
         super(props);
         this.board = React.createRef();
-        this.state = { };
+        this.state = {};
     }
 
     render() {
-        return <div className="mainContainer">
-            <div className="boardContainerCreate">
-                <Chessboard ref={this.board} />
+        return <div>
+            <div className="cameras">
+                <div className="camera">
+                    <div className="controls">
+                        <span className="input-group-text bg-transparent border-0 hidden" id="basic-addon1">
+                            <img src="../Assets/icons/microphone-solid.svg" style={{ width: 20, height: 20 }}></img>
+                        </span>
+                        <span className="input-group-text bg-transparent border-0 hidden" id="basic-addon1">
+                            <img src="../Assets/icons/video-solid.svg" style={{ width: 20, height: 20 }}></img>
+                        </span>
+                    </div>
+                </div>
+                <div className="camera"></div>
+                <div className="camera"></div>
+                <div className="camera"></div>
+                <div className="camera"></div>
+                <div className="camera"></div>
+                <div className="camera"></div>
+                <div className="camera"></div>
+                <div className="camera"></div>
+                <div className="camera"></div>
+                <div className="camera"></div>
+                <div className="camera"></div>
+                <div className="camera"></div>
             </div>
-            <div className="buttonsContainerCreate">
-                
+            <div className="maincontent">
+                <div className="boardContainer">
+                    <Chessboard ref={this.board} />
+                </div>
+                <div className="panel">
+
+                </div>
             </div>
         </div>;
     }
