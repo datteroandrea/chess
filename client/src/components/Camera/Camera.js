5036df08a98a624a96083a9807cdd123848b89d8
datteroandrea <dattero.andrea@gmail.com>
2022-03-25 19:00:53 +0100

    added Camera component

---
+++ client/src/components/Camera/Camera.js
@@ -0,0 +1,30 @@
+import './Camera.css';
+import React from "react";
+import { Component } from "react";
+
+export default class Camera extends Component {
+
+    constructor(props) {
+        super(props);
+        this.camera = React.createRef();
+    }
+
+    componentDidMount() {
+        this.camera.current.srcObject = this.props.stream;
+        this.camera.current.play();
+    }
+
+    render() {
+        return <video ref={this.camera} className="camera" muted={true}>
+            <div className="controls">
+                <span className="input-group-text bg-transparent border-0 hidden" id="basic-addon1">
+                    <img src="../Assets/icons/microphone-solid.svg" style={{ width: 20, height: 20 }}></img>
+                </span>
+                <span className="input-group-text bg-transparent border-0 hidden" id="basic-addon1">
+                    <img src="../Assets/icons/video-solid.svg" style={{ width: 20, height: 20 }}></img>
+                </span>
+            </div>
+        </video>;
+    }
+
+}
