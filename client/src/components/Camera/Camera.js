74746c51113e8be0d67045f59a55666ae9bdc481
datteroandrea <dattero.andrea@gmail.com>
2022-03-28 19:35:00 +0200

    added admin mute

--- client/src/components/Camera/Camera.js
+++ client/src/components/Camera/Camera.js
@@ -9,6 +9,7 @@ export default class Camera extends Component {
         this.camera = React.createRef();
         this.state = {};
         this.state.muted = this.props.muted;
+        this.state.adminMute = false;
     }
 
     componentDidMount() {
@@ -17,23 +18,28 @@ export default class Camera extends Component {
     }
 
     render() {
-        return  <div className='cameraAndControlsHolder'>
-                    <video className="camera" ref={this.camera} muted={this.state.muted}></video>
-                    <span className="control toggleMicrophone" onClick={e => this.toggleMicrophone(e)}/>
-                    <span className="control toggleCamera" onClick={e => this.toggleCamera(e)}/>
-                </div>;
+        return <div className='cameraAndControlsHolder'>
+            <video className="camera" ref={this.camera} muted={this.state.muted && !this.state.adminMute}></video>
+            <span className="control toggleMicrophone" onClick={e => this.toggleMicrophone(e)} />
+            <span className="control toggleCamera" onClick={e => this.toggleCamera(e)} />
+        </div>;
     }
 
     toggleMicrophone(e) {
         e.target.classList.toggle("toggle");
         this.state.muted = (e.target.classList).contains("toggle");
-        this.setState({ });
+        this.setState({});
     }
 
     toggleCamera(e) {
         e.target.classList.toggle("toggle");
         this.camera.current.srcObject = !(e.target.classList).contains("toggle") ? this.props.stream : null;
-        if(this.camera.current.srcObject) this.camera.current.play();
+        if (this.camera.current.srcObject) this.camera.current.play();
+    }
+
+    toggleAdminMute() {
+        this.state.adminMute = !this.state.adminMute;
+        this.setState({});
     }
 
 }
