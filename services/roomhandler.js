4bb99e1c5a3f99cbc6d2a50f3a040c1e26edeb72
datteroandrea <dattero.andrea@gmail.com>
2022-03-29 21:21:52 +0200

    added control if admin

--- services/roomhandler.js
+++ services/roomhandler.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const https = require('https');
 const path = require('path');
 const socket = require("socket.io");
+const Room = require("../models/room");
 
 const httpsServer = https.createServer({
     key: fs.readFileSync(path.join(__dirname, '../', 'key.pem')),
@@ -15,26 +16,52 @@ const io = socket(httpsServer);
 let room = { };
 
 io.on('connection', (socket) => {
-    socket.on("join-room", (roomId, userId)=>{
-        if(!room[roomId]) room[roomId] = { };
-        room[roomId][userId] = socket;
+    socket.on("join-room", async (roomId, userSessionId, userId)=>{
+        if(!room[roomId]) {
+            room[roomId] = { };
+        }
+        room[roomId][userSessionId] = socket;
+        room[roomId][userSessionId].userId = userId;
         socket.join(roomId);
         socket.to(roomId).emit('user-connected', userId);
 
-        socket.on("admin-mute", (mute) => {
+        socket.on("admin-mute", async (userId) => {
             // controlla se l'utente è admin della room attraverso una query e se si esegui l'emit
-            room[roomId][userId].emit("admin-mute");
+            let queryRoom = await Room.findOne({ roomId });
+            if(queryRoom.admins.includes(room[roomId][userSessionId].userId)) {
+                room[roomId][userSessionId].emit("admin-mute");
+            }
         });
 
-        socket.on('board-update', (position) => {
+        socket.on('board-update', async (position) => {
             // controlla se l'utente è admin della room attraverso una query e se si esegui l'emit
+            if(queryRoom.admins.includes(room[roomId][userSessionId].userId)) {
+                socket.to(roomId).emit('board-update', position);
+            }
         });
 
-        socket.on('toggle-stockfish', ()=>{
+        socket.on('toggle-stockfish', async ()=>{
             // controlla se l'utente è admin della room attraverso una query e se si esegui l'emit
+            if(queryRoom.admins.includes(room[roomId][userSessionId].userId)) {
+                socket.to(roomId).emit('toggle-stockfish', position);
+            }
         });
 
-        socket.on('disconnect', () => {
+        socket.on('comment', async (comment)=>{
+            // controlla se l'utente è admin della room attraverso una query e se si esegui l'emit
+            if(queryRoom.admins.includes(room[roomId][userSessionId].userId)) {
+                socket.to(roomId).emit('comment', comment);
+            }
+        });
+
+        socket.on('toggle-move', async ()=>{
+            // controlla se l'utente è admin della room attraverso una query e se si esegui l'emit
+            if(queryRoom.admins.includes(room[roomId][userSessionId].userId)) {
+                socket.to(roomId).emit('toggle-move');
+            }
+        });
+
+        socket.on('disconnect', async () => {
             socket.to(roomId).emit('user-disconnected', userId);
         });
     });
