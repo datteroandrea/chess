976786c7ae0d1b16abba5558d39ebee9a4810e3f
datteroandrea <dattero.andrea@gmail.com>
2022-04-16 22:31:29 +0200

    added elo in profile view

--- routes/profile.js
+++ routes/profile.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
+const Profile = require('../models/profile');
 const router = new express.Router();
 
 router.get('/', async (req,res)=>{
     let token = jwt.decode(req.token);
     let user = await User.findOne({ userId: token.userId });
-    return res.send(user);
+    let profile = await Profile.findOne({ profileId: token.userId });
+    return res.send({ email: user.email, username: user.username, elo: profile.elo });
 });
 
 module.exports = router;
